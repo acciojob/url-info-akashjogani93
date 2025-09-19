4ee3d46c09eda4d0afd1045eb4d0f69523c01747
Akash Jogani <114595432+akashjogani93@users.noreply.github.com>
2025-09-19 09:19:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+// alert(location.href)
+function checkURLLength()
+{
+	const currecturl=location.href;
+	const urllength=currecturl.length;
+	alert("The length of the URL is: " + urlLength)
+}
